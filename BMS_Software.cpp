9ee768b51b08232af089f7e5f2a53a2fcef3b1de
Jacob Lipina <jrlwd5@mst.edu>
2019-02-01 17:46:42 -0600

    Wrote functions to read values and set input & output pins

--- BMS_Software.cpp
+++ BMS_Software.cpp
@@ -29,6 +29,7 @@ RoveCommEthernetUdp RoveComm; //??What does this do Van?
 void getMainCurrent(RC_BMSBOARD_MAINIMEASmA_DATATYPE &main_current); //??func has & sign to pass value for main current back to program. Is it correct that it gets placed in "RC_BMSBOARD_BLAH main_current" var in loop?
 void getCellVoltage(RC_BMSBOARD_VMEASmV_DATATYPE cell_voltage[RC_BMSBOARD_VMEASmV_DATACOUNT]); //??How do the values in the array get back to the loop?
 void getBattTemp(RC_BMSBOARD_TEMPMEASmDEGC_DATATYPE &batt_temp);
+void getOutVoltage(uint16_t &pack_out_voltage);
 void setInputPins();
 void setOutputPins();
 void setOutputStates();
@@ -53,14 +54,14 @@ const uint32_t BUZZER_EN						= 2002; //
 #define RC_BMSBOARD_VMEASmV_DATACOUNT       	= 9 
 #define RC_BMSBOARD_VMEASmV_HEADER       		= RC_BMSBOARD_VMEASmV_DATAID,RC_BMSBOARD_VMEASmV_DATACOUNT
 #define RC_BMSBOARD_VMEASmV_PACKENTRY    		= 0     
-#define RC_BMSBOARD_VMEASmV_C1GENTRY     		= 1
-#define RC_BMSBOARD_VMEASmV_C21ENTRY     		= 2
-#define RC_BMSBOARD_VMEASmV_C32ENTRY     		= 3
-#define RC_BMSBOARD_VMEASmV_C43ENTRY     		= 4
-#define RC_BMSBOARD_VMEASmV_C54ENTRY     		= 5
-#define RC_BMSBOARD_VMEASmV_C65ENTRY     		= 6
-#define RC_BMSBOARD_VMEASmV_C76ENTRY     		= 7
-#define RC_BMSBOARD_VMEASmV_C87ENTRY     		= 8
+#define RC_BMSBOARD_VMEASmV_C1ENTRY     		= 1
+#define RC_BMSBOARD_VMEASmV_C2ENTRY     		= 2
+#define RC_BMSBOARD_VMEASmV_C3ENTRY     		= 3
+#define RC_BMSBOARD_VMEASmV_C4ENTRY     		= 4
+#define RC_BMSBOARD_VMEASmV_C5ENTRY     		= 5
+#define RC_BMSBOARD_VMEASmV_C6ENTRY     		= 6
+#define RC_BMSBOARD_VMEASmV_C7ENTRY     		= 7
+#define RC_BMSBOARD_VMEASmV_C8ENTRY     		= 8
 
 	// Battery Temperature
 #define RC_BMSBOARD_TEMPMEASmDEGC_DATAID       	= 02+_TYPE_TELEMETRY+_BMSBOARD_BOARDNUMBER
@@ -88,47 +89,49 @@ const int ROVECOMM_DELAY						= 10; //??Is there a better delay const in the man
 // Sensor Volts/Amps Readings Pins
 #define PACK_I_MEAS				= PE_0;
 #define PACK_V_MEAS				= PE_1;
-#define LOG_V_MEAS				= PE_2;
-#define TEMP_D_MEAS				= PM_3;
-#define C1_GND_MEAS				= PK_3;
-#define C2_C1_MEAS				= PK_2;
-#define C3_C2_MEAS				= PK_1;
-#define C4_C3_MEAS				= PK_0;
-#define C5_C4_MEAS				= PB_5;
-#define C6_C5_MEAS				= PB_4;
-#define C7_C6_MEAS				= PD_5;
-#define C8_C7_MEAS				= PD_4;
+#define LOGIC_V_MEAS			= PE_2;
+#define TEMP_degC_MEAS			= PM_3;
+#define C1_V_MEAS				= PK_3; //CELL 1 - GND
+#define C2_V_MEAS				= PK_2; //CELL 2 - CELL 1
+#define C3_V_MEAS				= PK_1; //CELL 3 - CELL 2
+#define C4_V_MEAS				= PK_0; //CELL 4 - CELL 3
+#define C5_V_MEAS				= PB_5; //CELL 5 - CELL 4
+#define C6_V_MEAS				= PB_4; //CELL 6 - CELL 5
+#define C7_V_MEAS				= PD_5; //CELL 7 - CELL 6
+#define C8_V_MEAS				= PD_4; //CELL 8 - CELL 7
 
 // Sensor Measurment: Constants and Calculations ////////////////////////////////////////////////
 //
 // Tiva1294C RoveBoard Specs
-const float VCC                  = 3.3;       //volts
-const float ADC_MAX            	 = 4096;      //bits
-const float ADC_MIN            	 = 0;         //bits
-const int IDLE_SHUTOFF_MINS		 = 30;
+const float VCC                 = 3.3;       //volts
+const float ADC_MAX            	= 4096;      //bits
+const float ADC_MIN            	= 0;         //bits
+const int IDLE_SHUTOFF_MINS		= 30;
 
 // ACS759ECB-200B-PFF-T Current Sensor Specs
 	// Find at: https://www.digikey.com/products/en?keywords=%20620-1466-5-ND%20
-const float SENSOR_SENSITIVITY   = 0.0066;    //volts/amp
-const float SENSOR_SCALE         = 0.5;
-const float SENSOR_BIAS          = VCC * SENSOR_SCALE;  //Viout voltage when current is at 0A (aka quiescent output voltage)
+const float SENSOR_SENSITIVITY  = 0.0066;    //volts/amp
+const float SENSOR_SCALE        = 0.5;
+const float SENSOR_BIAS         = VCC * SENSOR_SCALE;  //Viout voltage when current is at 0A (aka quiescent output voltage)
 														// Noise is 2mV, meaning the smallest current that the device is able to resolve is 0.3A
-const float CURRENT_MAX          = (VCC - SENSOR_BIAS - 0.33) / SENSOR_SENSITIVITY; // Amps
-const float CURRENT_MIN          = -(SENSOR_BIAS - 0.33) / SENSOR_SENSITIVITY; // Amps
-const float OVERCURRENT			 = 180; //TODO: This value should be lower, but where?
+const float CURRENT_MAX         = (VCC - SENSOR_BIAS - 0.33) / SENSOR_SENSITIVITY; // Amps
+const float CURRENT_MIN         = -(SENSOR_BIAS - 0.33) / SENSOR_SENSITIVITY; // Amps
+const float OVERCURRENT			= 180; //TODO: This value should be lower, but where?
 
 // Voltage Measurments
-const float VOLTS_MIN            = 0;
-const float PACK_VOLTS_MAX       = 33.6; //TODO: This num may change as we test using hardware
-const float CELL_VOLTS_MAX 		 = 4.2;
+const float VOLTS_MIN           = 0;
+const float PACK_VOLTS_MAX      = 33.6; //TODO: This num may change as we test using hardware
+const float CELL_VOLTS_MAX 		= 4.2;
 
 // TMP37 Temp Sensor Specs 
 	//Find at: https://www.digikey.com/products/en?mpart=TMP37FT9Z&v=505
 		//Voltage at 20 deg C is 500mV
 		//Scale Factor is 20mV/deg C
-const float	 
+const float	TEMP_MIN = 0;//TODO: test temp conversions with a thermometer.
+const float TEMP_MAX = 160;
 
 // Minimum and Maximum Values: Constants
+
 
 void setup()
 {
@@ -146,9 +149,11 @@ void loop()
 	RC_BMSBOARD_MAINIMEASmA_DATATYPE main_current;
 	RC_BMSBOARD_VMEASmV_DATATYPE cell_voltages[RC_BMSBOARD_VMEASmV_DATACOUNT]; //??"cell_voltages." Is the 's' there for a reason?
 	RC_BMSBOARD_TEMPMEASmDEGC_DATATYPE batt_temp;
-	
+	uint16_t pack_out_voltage;
+
 	getMainCurrent(main_current);
 	getCellVoltages(cell_voltages);
+	getOutVoltage(pack_out_voltage);
 	getBattTemp(batt_temp);
 	
 	RoveComm.write(RC_BMSBOARD_MAINIMEASmA_HEADER, main_current);
--- BMS_Software.h
+++ BMS_Software.h
@@ -1,9 +1,11 @@
 // Battery Managment System (BMS) Software /////////////////////////////////////////
 //
-// Created for 2019 Rover by: Jacob Lipina, jrlwd5
+// Created for 2019 Valkyrie by: Jacob Lipina, jrlwd5
 //
 //
 // Functions ///////////////////////////////////////////////////////////////////////
+
+// Anything multiplied by 1000 is done so to get the base unit (volt, Amp, degrees C) to milli (mV, mA, mDegC)
 
 void getMainCurrent(RC_BMSBOARD_MAINIMEASmA_DATATYPE &main_current)
 {
@@ -17,13 +19,97 @@ void getMainCurrent(RC_BMSBOARD_MAINIMEASmA_DATATYPE &main_current)
 
 void getCellVoltage(RC_BMSBOARD_VMEASmV_DATATYPE cell_voltage[RC_BMSBOARD_VMEASmV_DATACOUNT])
 {
-  for(int i = 0; i<RC_BMSBOARD_VMEASmV_DATACOUNT; i++)
-  {
-    cell_voltage[i]=i;
-  }
+ 	float adc_reading;
+ 	string CELL_MEAS_PINS[9] = {"PACK_V_MEAS", "C1_V_MEAS", "C2_V_MEAS", "C3_V_MEAS", 
+ 								"C4_V_MEAS", "C5_V_MEAS", "C6_V_MEAS", "C7_V_MEAS", "C8_V_MEAS"};
+
+ 	adc_reading = analogRead(CELL_MEAS_PINS[0]);	
+ 	cell_voltage[0] = 1000 * map(adc_reading, ADC_MIN, ADC_MAX, VOLTS_MIN, PACK_VOLTS_MAX);
+ 	
+ 	for(int i = 1; i<RC_BMSBOARD_VMEASmV_DATACOUNT; i++)
+ 	{
+ 	  if (i == 0)
+ 	  {
+ 	  	adc_reading = analogRead(CELL_MEAS_PINS[i])
+ 	  	cell_voltage[i] = 1000 * map(adc_reading, ADC_MIN, ADC_MAX, VOLTS_MIN, PACK_VOLTS_MAX);
+ 	  }
+ 	  else
+ 	  {
+ 	  	adc_reading = analogRead(CELL_MEAS_PINS[i])
+ 	  	cell_voltage[i] = 1000 * map(adc_reading, ADC_MIN, ADC_MAX, VOLTS_MIN, CELL_VOLTS_MAX);
+ 	  } //end if
+ 	  return;
+ 	} //end for
+}
+
+void getOutVoltage(uint16_t &pack_out_voltage);
+{
+	float adc_reading;
+
+  	adc_reading = analogRead(PACK_V_MEAS);
+  	pack_out_voltage = 1000 * map(adc_reading, ADC_MIN, ADC_MAX, VOLTS_MIN, PACK_VOLTS_MAX);
+
+  	return;
 }
 
 void getBattTemp(RC_BMSBOARD_TEMPMEASmDEGC_DATATYPE &batt_temp)
 {
+	float adc_reading;
 
+	adc_reading = analogRead(TEMP_degC_MEAS);
+	batt_temp = 1000 * map(adc_reading, ADC_MIN, ADC_MAX, TEMP_MIN, TEMP_MAX);
+
+	return;
+}
+
+void setInputPins()
+{
+	pinMode(PACK_I_MEAS, 	INPUT);
+	pinMode(PACK_V_MEAS, 	INPUT);
+	pinMode(LOGIC_V_MEAS, 	INPUT);
+	pinMode(TEMP_degC_MEAS, INPUT);
+	pinMode(C1_V_MEAS, 		INPUT);
+	pinMode(C2_V_MEAS, 		INPUT);
+	pinMode(C3_V_MEAS, 		INPUT);
+	pinMode(C4_V_MEAS, 		INPUT);
+	pinMode(C5_V_MEAS, 		INPUT);
+	pinMode(C6_V_MEAS, 		INPUT);
+	pinMode(C7_V_MEAS, 		INPUT);
+	pinMode(C8_V_MEAS, 		INPUT);
+
+	return;
+}
+
+void setOutputPins()
+{
+	pinMode(PACK_OUT_CTR, 		OUTPUT);
+	pinMode(PACK_OUT_IND, 		OUTPUT);
+	pinMode(LOGIC_SWITCH_CTR, 	OUTPUT);
+	pinMode(BUZZER_CTR, 		OUTPUT);
+	pinMode(FAN_1_CTR, 			OUTPUT);
+	pinMode(FAN_2_CTR, 			OUTPUT);
+	pinMode(FAN_3_CTR, 			OUTPUT);
+	pinMode(FAN_4_CTR, 			OUTPUT);
+	pinMode(FAN_PWR_IND, 		OUTPUT);
+	pinMode(SW_IND, 			OUTPUT);
+	pinMode(SW_ERR, 			OUTPUT);
+
+	return;
+}
+
+void setOutputStates()
+{
+	digitalWrite(PACK_OUT_CTR, 		LOW);
+	digitalWrite(PACK_OUT_IND, 		LOW);
+	digitalWrite(LOGIC_SWITCH_CTR, 	LOW);
+	digitalWrite(BUZZER_CTR, 		LOW);
+	digitalWrite(FAN_1_CTR, 		LOW);
+	digitalWrite(FAN_2_CTR, 		LOW);
+	digitalWrite(FAN_3_CTR, 		LOW);
+	digitalWrite(FAN_4_CTR, 		LOW);
+	digitalWrite(FAN_PWR_IND, 		LOW);
+	digitalWrite(SW_IND, 			LOW);
+	digitalWrite(SW_ERR, 			LOW);
+	
+	return;
 }
