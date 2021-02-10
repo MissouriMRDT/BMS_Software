bb44da2c8261ac954822449cd91f04020e051628
Max Ryan <70597225+MaxR-mrdt@users.noreply.github.com>
2021-02-10 14:10:51 -0600

    began defining TIVA pins in header file

--- BMS_software_main/BMS_software_main.h
+++ BMS_software_main/BMS_software_main.h
@@ -1,2 +1,30 @@
 #include <Energia.h>
 #include "RoveComm.h"
+//sets up RoveComm
+
+//LED control pins
+#define SW_IND              PF_1
+#define SW_ERR_IND
+#define V_OUT_IND
+#define FANS_IND
+
+//Sensor pins
+#define TEMP_SENSE 
+#define V_OUT_SENSE
+#define PACK_I_SENSE
+#define LOGIC_POWER_SENSE
+
+//Cell voltages
+#define CELL1_VOLTAGE
+#define CELL2_VOLTAGE
+#define CELL3_VOLTAGE
+#define CELL4_VOLTAGE
+#define CELL5_VOLTAGE
+#define CELL6_VOLTAGE
+#define CELL7_VOLTAGE
+#define CELL8_VOLTAGE
+
+//Control 
+#define BUZZER_CONTROL
+#define FAN_CONTROL
+#define PACK_GATE
