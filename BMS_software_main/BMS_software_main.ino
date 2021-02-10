c726722479498300f89aca34b8d5fac956a644e7
Max Ryan <70597225+MaxR-mrdt@users.noreply.github.com>
2021-02-10 15:09:29 -0600

    created setPinInputs and setPinOutputs functions
    set pinModes for Cell voltage and Sensor pins as inputs

--- BMS_software_main/BMS_software_main.ino
+++ BMS_software_main/BMS_software_main.ino
@@ -1,3 +1,4 @@
+//code that will actually run
 void setup(){
 
 Serial.begin(9600);
@@ -5,5 +6,28 @@ Serial.begin(9600);
 }
 
 void loop(){
+
+}
+
+//functions that will be called
+void setPinInputs(){
+
+pinMode(CELL1_VOLTAGE_PIN,      INPUT)
+pinMode(CELL2_VOLTAGE_PIN,      INPUT)
+pinMode(CELL3_VOLTAGE_PIN,      INPUT)
+pinMode(CELL4_VOLTAGE_PIN,      INPUT)
+pinMode(CELL5_VOLTAGE_PIN,      INPUT)
+pinMode(CELL6_VOLTAGE_PIN,      INPUT)
+pinMode(CELL7_VOLTAGE_PIN,      INPUT)
+pinMode(CELL8_VOLTAGE_PIN,      INPUT)
+
+pinMode(TEMP_SENSE_PIN,         INPUT)
+pinMode(V_OUT_SENSE_PIN,        INPUT)
+pinMode(PACK_I_SENSE_PIN,       INPUT)
+pinMode(LOGIC_POWER_SENSE_PIN,  INPUT)
+
+}
+
+void setPinOutputs(){
 
 }
