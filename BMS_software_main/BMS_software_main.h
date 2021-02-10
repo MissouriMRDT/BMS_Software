f72f4b5e594ce92a20a1d432ccd9518baf54a777
Max Ryan <70597225+MaxR-mrdt@users.noreply.github.com>
2021-02-10 14:54:14 -0600

    added _PIN to specify these are the pin names
    began serial monitor 9600

--- BMS_software_main/BMS_software_main.h
+++ BMS_software_main/BMS_software_main.h
@@ -3,30 +3,30 @@
 //sets up RoveComm
 
 //LED control pins
-#define SW_IND              PF_1
-#define SW_ERR_IND          PM_3
-#define V_OUT_IND           PH_2
-#define FANS_IND            PH_3
+#define SW_IND_PIN              PF_1
+#define SW_ERR_IND_PIN          PM_3
+#define V_OUT_IND_PIN           PH_2
+#define FANS_IND_PIN            PH_3
 
 //Sensor pins
-#define TEMP_SENSE          PD_1
-#define V_OUT_SENSE         PD_0
-#define PACK_I_SENSE        PN_4
-#define LOGIC_POWER_SENSE   PK_7
+#define TEMP_SENSE_PIN          PD_1
+#define V_OUT_SENSE_PIN         PD_0
+#define PACK_I_SENSE_PIN        PN_4
+#define LOGIC_POWER_SENSE_PIN   PK_7
 
 //Cell voltages
-#define CELL1_VOLTAGE       PK_3
-#define CELL2_VOLTAGE       PK_2
-#define CELL3_VOLTAGE       PK_1
-#define CELL4_VOLTAGE       PK_0
-#define CELL5_VOLTAGE       PB_5
-#define CELL6_VOLTAGE       PB_4
-#define CELL7_VOLTAGE       PD_5
-#define CELL8_VOLTAGE       PD_4
+#define CELL1_VOLTAGE_PIN       PK_3
+#define CELL2_VOLTAGE_PIN       PK_2
+#define CELL3_VOLTAGE_PIN       PK_1
+#define CELL4_VOLTAGE_PIN       PK_0
+#define CELL5_VOLTAGE_PIN       PB_5
+#define CELL6_VOLTAGE_PIN       PB_4
+#define CELL7_VOLTAGE_PIN       PD_5
+#define CELL8_VOLTAGE_PIN       PD_4
 
 //Control 
-#define BUZZER_CONTROL      PL_0
-#define FAN_CONTROL         PP_5
-#define PACK_GATE           PM_6
-#define LCD_TX              PA_7
-#define LOGIC_SWITCH        PK_6
+#define BUZZER_CONTROL_PIN      PL_0
+#define FAN_CONTROL_PIN         PP_5
+#define PACK_GATE_PIN           PM_6
+#define LCD_TX_PIN              PA_7
+#define LOGIC_SWITCH_PIN        PK_6
--- BMS_software_main/BMS_software_main.ino
+++ BMS_software_main/BMS_software_main.ino
@@ -1,7 +1,9 @@
 void setup(){
 
+Serial.begin(9600);
+
 }
 
 void loop(){
-    
+
 }
