85d197cca843ce5230fedfb3a8f43fb0b901e281
Max Ryan <70597225+MaxR-mrdt@users.noreply.github.com>
2021-03-23 20:32:25 -0500

    changed header to include proper pin values
    also created and started ADC excel sheet

---
+++ BMStest/BMStest.h
@@ -0,0 +1,101 @@
+#include <Energia.h>
+#ifndef BMStest
+#define BMStest
+
+#define BUZZER_CTRL_PIN        PL_0    // change after new tiva
+#define FAN_CTRL_PIN           PP_5
+#define PACK_OUT_CTRL_PIN      PM_6
+#define LOGIC_SWITCH_CTRL_PIN  PK_6
+
+//Indicator Pins
+#define FAN_PWR_IND_PIN       PH_3    // change after new tiva
+#define SW_IND_PIN            PF_1
+#define SW_ERR_IND_PIN        PM_3
+
+
+//Sensor Volts/Amps Reading Pins
+#define PACK_I_MEAS_PIN       PN_4
+#define PACK_V_MEAS_PIN       PD_0    //Measures voltage outputting to rover. Will read zero if PACK_OUT_CTL_PIN is LOW.
+#define LOGIC_V_MEAS_PIN      PK_7   //Measures total voltage of pack continuously while logic switch is on. 
+#define TEMP_degC_MEAS_PIN    PD_1
+
+//Cell Voltage Pins
+#define C1_V_MEAS_PIN         PK_3    //CELL 1 - GND
+#define C2_V_MEAS_PIN         PK_2    //CELL 2 - CELL 1
+#define C3_V_MEAS_PIN         PK_1    //CELL 3 - CELL 2
+#define C4_V_MEAS_PIN         PK_0    //CELL 4 - CELL 3
+#define C5_V_MEAS_PIN         PB_5    //CELL 5 - CELL 4
+#define C6_V_MEAS_PIN         PB_4    //CELL 6 - CELL 5
+#define C7_V_MEAS_PIN         PD_5    //CELL 7 - CELL 6
+#define C8_V_MEAS_PIN         PD_4    //CELL 8 - CELL 7
+const int CELL_MEAS_PINS[] = {C1_V_MEAS_PIN,C2_V_MEAS_PIN,C3_V_MEAS_PIN,C4_V_MEAS_PIN,C5_V_MEAS_PIN,C6_V_MEAS_PIN,C7_V_MEAS_PIN,C8_V_MEAS_PIN};
+
+//Voltage measurements
+#define PACK_VOLTS_MIN           0      //mV
+#define CELL_VOLTS_MIN        2400   //mV
+#define PACK_VOLTS_MAX        33600  //mV
+#define CELL_VOLTS_MAX        4200   //mV
+#define PACK_UNDERVOLTAGE     21600  //mV
+#define PACK_LOWVOLTAGE       25000  //mV
+#define PACK_SAFETY_LOW       PACK_UNDERVOLTAGE - 4000 //mV
+#define CELL_UNDERVOLTAGE     2650   //mV
+#define PACK_EFFECTIVE_ZERO   5000   //mV
+#define CELL_EFFECTIVE_ZERO   1000   //mV
+#define PACK_V_ADC_MIN        0      //bits
+#define PACK_V_ADC_MAX        4096   //bits 
+#define CELL_V_ADC_MIN        1140   //bits2755,2930
+#define CELL_V_ADC_MAX        4270   //bits3450,3245
+
+//Current Measurements
+#define CURRENT_MAX           20000 //mA
+#define CURRENT_MIN           -20000. //mA
+#define OVERCURRENT           10000 //mA
+#define CURRENT_ADC_MIN       0      //bits
+#define CURRENT_ADC_MAX       4096   //bits
+
+//temperature measurements
+#define TEMP_MIN              0      //mdeg C
+#define TEMP_MAX              160000 //mdeg
+#define TEMP_THRESHOLD        38000  //mdeg C
+#define TEMP_ADC_MIN          0      //bits 
+#define TEMP_ADC_MAX          4096   //bits
+#define NUM_TEMP_AVERAGE      10     //batt_temp will be average of this many measurements
+
+float cell_voltage[8];
+
+//Average 
+float cell_adc_average[8];
+int count;
+int countMax;
+float pack_adc_v_average;
+float pack_adc_i_average;
+float temp_adc_average;
+float pack_voltage;
+
+
+int beep_time = 3000;
+
+void setInputPins();
+
+void setOutputPins();
+
+void setOutputStates();
+
+void getCellVoltage(float cell_voltage[],int count, float &adc_average, float pack_voltage);
+
+void getPackVoltage(float pack_adc_v_average, int count, float pack_voltage);
+
+void getPackCurrent(float pack_adc_i_average);
+
+void getTemperature(float temp_adc_average,int count);
+
+void initTest(int beep_time);
+
+void beep(int beep_time);
+
+void turnOffFans();
+
+void turnOnFans();
+
+
+#endif
---
+++ BMStest/BMStest.ino
@@ -0,0 +1,219 @@
+#include "BMStest.h"
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  setInputPins();
+  setOutputPins();
+  setOutputStates();
+  //initTest(beep_time);
+  //Serial.println("Pack Current:");
+  
+  int count = 0;
+  int countMax = 20;
+  //float pack_voltage = 0;
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  getCellVoltage(cell_voltage,count,cell_adc_average,pack_voltage);
+ //getPackVoltage(pack_adc_v_average, count, pack_voltage);
+ //getPackCurrent(pack_adc_i_average);
+ //getTemperature(temp_adc_average, count);
+ 
+ count++;
+ if(count == 21)
+ {
+  count = 0;
+ }
+ //delay(30);  
+}
+
+void setInputPins()
+{
+  pinMode(PACK_I_MEAS_PIN,      INPUT);
+  pinMode(PACK_V_MEAS_PIN,      INPUT);
+  pinMode(LOGIC_V_MEAS_PIN,     INPUT);
+  pinMode(TEMP_degC_MEAS_PIN,   INPUT);
+  pinMode(C1_V_MEAS_PIN,        INPUT);
+  pinMode(C2_V_MEAS_PIN,        INPUT);
+  pinMode(C3_V_MEAS_PIN,        INPUT);
+  pinMode(C4_V_MEAS_PIN,        INPUT);
+  pinMode(C5_V_MEAS_PIN,        INPUT);
+  pinMode(C6_V_MEAS_PIN,        INPUT);
+  pinMode(C7_V_MEAS_PIN,        INPUT);
+  pinMode(C8_V_MEAS_PIN,        INPUT);
+  Serial.println("setInputPins Complete");
+}
+
+void setOutputPins()
+{
+  pinMode(PACK_OUT_CTRL_PIN,     OUTPUT);
+  pinMode(LOGIC_SWITCH_CTRL_PIN, OUTPUT);
+  pinMode(BUZZER_CTRL_PIN,       OUTPUT);
+  pinMode(FAN_CTRL_PIN,          OUTPUT);
+  pinMode(FAN_PWR_IND_PIN,      OUTPUT);
+  pinMode(SW_IND_PIN,           OUTPUT);
+  pinMode(SW_ERR_IND_PIN,       OUTPUT);
+  Serial.println("setOutputPins Complete");
+  return;
+}
+
+void setOutputStates()
+{
+  digitalWrite(PACK_OUT_CTRL_PIN,      HIGH);
+  digitalWrite(LOGIC_SWITCH_CTRL_PIN,  LOW);
+  digitalWrite(BUZZER_CTRL_PIN,        LOW);
+  digitalWrite(FAN_CTRL_PIN,           LOW);
+  digitalWrite(FAN_PWR_IND_PIN,       LOW);
+  digitalWrite(SW_IND_PIN,            LOW);
+  digitalWrite(SW_ERR_IND_PIN,        LOW);
+  Serial.println("setOutputStates Complete");
+  return;
+}
+
+void getCellVoltage(float cell_voltage[],int count, float cell_adc_average[], float pack_voltage)
+{
+  for(int i = 0; i < 8 ; i++)
+  {
+    int adc_reading = analogRead(CELL_MEAS_PINS[i]);   //Serial.println(analogRead(CELL_MEAS_PINS[1]));
+    cell_adc_average[i]+= adc_reading; // adds cell voltages to an array of 8 (for average to be taken later)
+  }
+  if (count == 20)
+  {
+    pack_voltage = 0;
+    //take average
+    for(int i = 0; i < 8 ; i++)
+    { 
+      cell_adc_average[i]= cell_adc_average[i]/(count + 1);
+
+      cell_voltage[i]= map(cell_adc_average[i],CELL_V_ADC_MIN, CELL_V_ADC_MAX, CELL_VOLTS_MIN, CELL_VOLTS_MAX);
+
+      pack_voltage += cell_voltage[i];
+    }
+    
+    //prints a table of adc and voltage average values
+    Serial.println("CELL");
+    Serial.print("ADC:");
+    Serial.print("      ");
+    Serial.print("VOLTS:");
+    Serial.println();
+    for(int i = 0; i < 8 ; i++) 
+    {
+      Serial.print(cell_adc_average[i]);
+      Serial.print("      ");
+      Serial.print(cell_voltage[i]);
+      Serial.println();
+    }
+    Serial.println();
+  }
+  return;
+}
+
+void getPackVoltage(float pack_adc_v_average, int count, float pack_voltage)
+{
+  if (count == 20)
+  { 
+    Serial.println("PACK VOLTAGE");
+    Serial.print("VOLTS:");
+    Serial.println();
+    Serial.print(pack_voltage);
+    Serial.println();
+  }
+  return;
+}
+ 
+void getPackCurrent(float pack_adc_i_average)
+{
+  int adc_reading = analogRead(PACK_I_MEAS_PIN);
+  //pack_adc_i_average += adc_reading;
+  Serial.println("adc:");
+  Serial.println(analogRead(PACK_I_MEAS_PIN));
+  int mapped_current = map(adc_reading,CURRENT_ADC_MIN,CURRENT_ADC_MAX,CURRENT_MIN,CURRENT_MAX);
+  Serial.println("map:");
+  Serial.println(mapped_current);
+  /*
+  if (count == 20)
+  {
+    pack_adc_i_average = pack_adc_i_average/(count + 1);
+    int pack_current = map(pack_adc_i_average, CURRENT_ADC_MIN, CURRENT_ADC_MAX, CURRENT_MIN, CURRENT_MAX);
+    
+    Serial.println("PACK CURRENT");
+    Serial.print("ADC:");
+    Serial.print("      ");
+    Serial.print("CURRENT:");
+    Serial.println();
+    Serial.print(pack_adc_i_average);
+    Serial.print("      ");
+    Serial.print(pack_current);
+    Serial.println();
+    
+  }*/
+  return;
+}
+
+void getTemperature(float temp_adc_average, int count)
+{
+  int adc_reading = analogRead(TEMP_degC_MEAS_PIN);
+  temp_adc_average += adc_reading;
+
+  if (count == 20)
+  {
+    temp_adc_average = temp_adc_average/(count+1);
+    int temp_average = map(temp_adc_average, TEMP_ADC_MIN, TEMP_ADC_MAX, TEMP_MIN, TEMP_MAX);
+
+    Serial.println("TEMPERATURE");
+    Serial.print("ADC:");
+    Serial.print("      ");
+    Serial.print("CELSUIS:");
+    Serial.println();
+    Serial.print(temp_adc_average);
+    Serial.print("      ");
+    Serial.print(temp_average);
+    Serial.println();
+  }
+  return;
+}
+
+void initTest(int beep_time)
+{
+  turnOnFans();
+  delay(1000);
+  turnOffFans();
+  //beep(beep_time);
+  Serial.println("Rover Initialized");
+  digitalWrite(FAN_PWR_IND_PIN,       HIGH);
+  delay(beep_time);
+  digitalWrite(SW_IND_PIN,            HIGH);
+  delay(beep_time);
+  digitalWrite(SW_ERR_IND_PIN,        HIGH);
+  delay(beep_time);
+  digitalWrite(FAN_PWR_IND_PIN,       LOW);
+  delay(beep_time);
+  digitalWrite(SW_IND_PIN,            LOW);
+  delay(beep_time);
+  digitalWrite(SW_ERR_IND_PIN,        LOW);
+  
+}
+void turnOnFans()
+{
+ digitalWrite(FAN_CTRL_PIN, HIGH);
+ digitalWrite(FAN_PWR_IND_PIN, HIGH);
+}
+
+void turnOffFans()
+{
+  digitalWrite(FAN_CTRL_PIN, LOW);
+  digitalWrite(FAN_PWR_IND_PIN, LOW);
+}
+
+void beep(int beep_time)
+{
+  digitalWrite(BUZZER_CTRL_PIN, HIGH);
+  delay(beep_time);
+  digitalWrite(BUZZER_CTRL_PIN, LOW);
+}
