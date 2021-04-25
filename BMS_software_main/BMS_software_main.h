41be8b794a93c6a7c6725f645e2457b95deaf2f0
Max Ryan <70597225+MaxR-mrdt@users.noreply.github.com>
2021-04-25 12:46:40 -0500

    changed pin variables to const uint8_t, inserted example mapping code to ino file

--- BMS_software_main/BMS_software_main.h
+++ BMS_software_main/BMS_software_main.h
@@ -3,33 +3,34 @@
 //sets up RoveComm
 
 //LED control pins
-#define SW_IND_PIN              PF_1
-#define SW_ERR_IND_PIN          PM_3
-#define V_OUT_IND_PIN           PH_2
-#define FANS_IND_PIN            PH_3
+const uint8_t SW_IND_PIN              PF_1;
+const uint8_t SW_ERR_IND_PIN          PM_3;
+const uint8_t V_OUT_IND_PIN           PH_2;
+const uint8_t FANS_IND_PIN            PH_3;
 
 //Sensor pins
-#define TEMP_SENSE_PIN          PD_1
-#define V_OUT_SENSE_PIN         PD_0
-#define PACK_I_SENSE_PIN        PN_4
-#define LOGIC_POWER_SENSE_PIN   PK_7
+const uint8_t TEMP_SENSE_PIN          PD_1;
+const uint8_t V_OUT_SENSE_PIN         PD_0;
+const uint8_t PACK_I_SENSE_PIN        PN_4;
+const uint8_t LOGIC_POWER_SENSE_PIN   PK_7;
 
 //Cell voltage pins
-#define CELL1_VOLTAGE_PIN       PK_3
-#define CELL2_VOLTAGE_PIN       PK_2
-#define CELL3_VOLTAGE_PIN       PK_1
-#define CELL4_VOLTAGE_PIN       PK_0
-#define CELL5_VOLTAGE_PIN       PB_5
-#define CELL6_VOLTAGE_PIN       PB_4
-#define CELL7_VOLTAGE_PIN       PD_5
-#define CELL8_VOLTAGE_PIN       PD_4
+const uint8_t CELL1_VOLTAGE_PIN       PK_3;
+const uint8_t CELL2_VOLTAGE_PIN       PK_2;
+const uint8_t CELL3_VOLTAGE_PIN       PK_1;
+const uint8_t CELL4_VOLTAGE_PIN       PK_0;
+const uint8_t CELL5_VOLTAGE_PIN       PB_5;
+const uint8_t CELL6_VOLTAGE_PIN       PB_4;
+const uint8_t CELL7_VOLTAGE_PIN       PD_5;
+const uint8_t CELL8_VOLTAGE_PIN       PD_4;
+const uint8_t cell_meas_pins[]=       {CELL1_VOLTAGE_PIN,CELL2_VOLTAGE_PIN,CELL3_VOLTAGE_PIN,CELL4_VOLTAGE_PIN,CELL5_VOLTAGE_PIN,CELL6_VOLTAGE_PIN,CELL7_VOLTAGE_PIN,CELL8_VOLTAGE_PIN}
 
 //Control 
-#define BUZZER_CONTROL_PIN      PL_0
-#define FAN_CONTROL_PIN         PP_5
-#define PACK_GATE_PIN           PM_6
-#define LCD_TX_PIN              PA_7
-#define LOGIC_SWITCH_PIN        PK_6
+const uint8_t BUZZER_CONTROL_PIN=      PL_0;
+const uint8_t FAN_CONTROL_PIN=         PP_5;
+const uint8_t PACK_GATE_PIN=           PM_6;
+const uint8_t LCD_TX_PIN=              PA_7;
+const uint8_t LOGIC_SWITCH_PIN=        PK_6;
 
 float cell1_voltage;
 
--- BMS_software_main/BMS_software_main.ino
+++ BMS_software_main/BMS_software_main.ino
@@ -50,7 +50,44 @@ void setPinOutputs()
     pinMode(LOGIC_SWITCH_PIN,       OUTPUT);
 }
 
+void setPinOutputStates()
+{
+    digitalWrite(BUZZER_CONTROL_PIN,    LOW);
+    digitalWrite(FAN_CONTROL_PIN,       LOW);
+    digitalWrite(PACK_GATE_PIN,         HIGH);
+    digitalWrite(LOGIC_SWITCH_PIN,      LOW);
+    digitalWrite(SW_IND_PIN,            LOW);
+    digitalWrite(SW_ERR_IND_PIN,        LOW);
+    digitalWrite(V_OUT_IND_PIN,         LOW);
+    digitalWrite(FANS_IND_PIN,          LOW);
+}
+
 void cellVoltageRead()
 {
     
 }
+
+/*
+float* getCellVoltages()
+{
+  float cells[RC_BMSBOARD_CELLV_MEAS_DATA_COUNT] = {};
+
+  for a in range(RC_BMSBOARD_CELLV_MEAS_DATA_COUNT)
+  {
+    float cell = map(analogRead(CELL_MEAS_PINS[a]), PACK_V_ADC_MIN, PACK_V_ADC_MAX, VOLTS_MIN, PACK_VOLTS_MAX);
+    // check errors
+    if(cell =< CELL_UNDERVOLTAGE)
+    {
+      delay(DEBOUNCE_DELAY);
+      float cell = map(analogRead(CELL_MEAS_PINS[a]), PACK_V_ADC_MIN, PACK_V_ADC_MAX, VOLTS_MIN, PACK_VOLTS_MAX);
+      if (cell =< CELL_UNDERVOLTAGE)
+      {
+        uint8_t undervoltcell = a;  
+        RoveComm.write(RC_BMSBOARD_CELLUNDERVOLTAGE_DATA_ID, undervoltcell);
+      }
+    }
+    cells[a] = cell/1000;
+  }
+  return float cells[RC_BMSBOARD_CELLV_MEAS_DATA_COUNT];
+
+*/
