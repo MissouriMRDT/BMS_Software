9d411fabe96ad1cdc1294d9f4501af24f2e28326
TylerNguyen03 <92827445+TylerNguyen03@users.noreply.github.com>
2023-11-15 17:05:34 -0600

    did pin assignments

---
+++ PinAssignments.h
@@ -0,0 +1,40 @@
+#ifndef PINASSIGNMENTS_H
+#define PINASSIGNMENTS_H
+
+
+// BUZZER
+#define BUZZER 34
+
+// Gate
+#define GATE 35
+
+// Error LED
+#define ERR_LED 33
+
+
+// Cell Sense
+#define CELL_SENSE_1 18
+#define CELL_SENSE_2 19
+#define CELL_SENSE_3 14
+#define CELL_SENSE_4 15
+#define CELL_SENSE_5 40
+#define CELL_SENSE_6 41
+#define CELL_SENSE_7 39
+#define CELL_SENSE_8 38
+
+// Temperature sensor
+#define TEMP 17
+
+// LCD
+#define LCD_TX 20
+
+// Logic Switch
+#define LOGIC_SWITCH_INPUT 22
+
+// Current Sense
+#define CURRENT_SENSE 24
+
+// Fan Teensy
+#define FAN 23
+
+#endif /* PINASSIGNMENTS_H */
