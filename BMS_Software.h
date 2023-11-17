81fe4e0c94fb98910d3fb12294a166aeaa37adc3
Vexas5266 <wd.grove2@gmail.com>
2023-11-16 20:57:46 -0600

    Added Errors, Finished Temp, Battery Current

--- BMS_Software.h
+++ BMS_Software.h
@@ -13,34 +13,56 @@ RoveCommEthernet RoveComm;
 IntervalTimer Telemetry;
 void telemetry();
 
+float mapAnalog();
 
-// State Variables
+#define MAX_TEMP 65
+#define CELL_UNDERVOLT_THRESHOLD 2.7
+#define CELL_CRITICAL_THRESHOLD 2.5
+#define MAX_CURRENT 80
 
-float temp; // degrees C
-
-#define ROOM_TEMP_C 0 //celcius
+float temp = 0;
+#define ROOM_TEMP_C 21 //celcius
 #define ROOM_TEMP_ANALOG 0
 #define OTHER_TEMP_C 0
 #define OTHER_TEMP_ANALOG 0
 
-void roverOff() {
-    beep // also check
-    delay(1000) //check with malikai
-    digitalWrite(LOGIC_SWITCH_INPUT, LOW)
-}
+float current = 0;
+#define ZERO_CURRENT 0 //Amps
+#define ZERO_CURRENT_ANALOG 0
+#define OTHER_CURRENT 0
+#define OTHER_CURRENT_ANALOG 0
 
-void roverRestart() {
-    digitalWrite(LOGIC_SWITCH_INPUT, HIGH)
-    beep // also check
-    delay(1000) //check with malikai
-    digitalWrite(LOGIC_SWITCH_INPUT, LOW)
-    off beep // turn off buzzer
-}
+float cell_voltages[8];
+uint8_t cell_voltage_pins[8] = {CELL_SENSE_1, CELL_SENSE_2, CELL_SENSE_3, CELL_SENSE_4, CELL_SENSE_5, CELL_SENSE_6, CELL_SENSE_7, CELL_SENSE_8};
+#define ZERO_VOLTS 0 //Volts
+#define ZERO_VOLTS_ANALOG 0
+#define OTHER_VOLTS 0
+#define OTHER_VOLTS_ANALOG 0 
 
-void roverRestart() {
-    beep 
-    delay(1000)
-    turn off contactor
-}
+void eStop();
+
+void roverRestart();
+
+void roverSuicide();
+
+
+uint32_t lastOvercurrentErrorTimeStamp = 0;
+#define TENTHOUSAND 10000 //BEMIS
+
+void errorOvercurrent();
+
+void errorCellUndervoltage();
+
+void errorCellCritical();
+
+#define NOTIFYOVERHEAT_LENGTH 6
+uint32_t lastBuzzTimeStamp = 0;
+uint32_t lastOverheatWriteTimeStamp = 0;
+uint16_t notifyOverheat[NOTIFYOVERHEAT_LENGTH] = {50, 50, 50, 50, 50, 30000};
+uint8_t notifyOverheatIndex = 0;
+
+void errorOverHeat();
 
 #endif /* TRAININGSOFTWARE_H */
+
+//temp cutoff 65 C
--- BMS_Software.ino
+++ BMS_Software.ino
@@ -12,72 +12,7 @@ void setup () {
 
 }
 
-uint16_t notifyOverCurrent[] = {500, 100, 250, 30000};
-uint8_t notify_index = 0;
-
-uint16_t notifyOverCurrent[] = {500, 100, 250, 30000};
-uint8_t notify_index = 0;
-
-uint16_t notifyOverCurrent[] = {500, 100, 250, 30000};
-uint8_t notify_index = 0;
-
-uint16_t notifyOverCurrent[] = {500, 100, 250, 30000};
-uint8_t notify_index = 0;
-
-void notifyError() {
-    if (overcurrent) {
-        if (current_time - last_buzz_time > notifyOverCurrent[notify_index]) {
-            if (i%2 == 0) {
-                digitalWrite(BUZZER, HIGH);
-            } else {
-                digitalWrite(BUZZER, LOW);
-            }
-            last_buzz_time = current_time;
-            notify_index++;
-        }
-    }
-
-    // repeat for other errors
-}
-
-void notifyLowVoltage() // below 2.7 voltage
-{
-    if (LowVoltage) {
-            if (current_time - last_buzz_time > notifyOverCurrent[notify_index]) {
-                if (i%2 == 0) {
-                    digitalWrite(BUZZER, HIGH);
-                } else {
-                    digitalWrite(BUZZER, LOW);
-                }
-                last_buzz_time = current_time;
-                notify_index++;
-            }
-        }
-
-    // repeat for other errors
-}
-
-
-void notifyUnderVoltage() // below 2.5 voltage
-{
-    if (UnderVoltage) {
-                if (current_time - last_buzz_time > notifyOverCurrent[notify_index]) {
-                    if (i%2 == 0) {
-                        digitalWrite(BUZZER, HIGH);
-                    } else {
-                        digitalWrite(BUZZER, LOW);
-                    }
-                    last_buzz_time = current_time;
-                    notify_index++;
-                }
-            }
-}
-
 void loop() {
-    //update temp reading
-    void readBatterTemp(){
-        float temp = analogRead(ROOM_TEMP);
-    }
     
     // turn off rover, estop
     rovecomm_packet packet = RoveComm.read();
@@ -87,7 +22,111 @@ void loop() {
         {
             int16_t data = *((int16_t*) packet.data);
             
-            roverOff(data);
+            eStop(data);
             break;
         }
+    }
+
+    current = mapAnalog(CURRENT_SENSE, ZERO_CURRENT, MAX_CURRENT, ZERO_CURRENT_ANALOG, MAX_CURRENT_ANALOG);
+    //Check for Overcurrent
+    if (current>=MAX_CURRENT) {
+        errorOvercurrent();
+    }
+
+    temp - mapAnalog(TEMP, ROOM_TEMP_C, OTHER_TEMP_C, ROOM_TEMP_ANALOG, OTHER_TEMP_ANALOG);
+    //Check for Overheat
+    if (temp >= MAX_TEMP) {
+        errorOverHeat();
+    } else {
+        digitalWrite(BUZZER, LOW);
+    }
+
+    //Check for Cell Undervoltage and Cell Critical
+    for (uint8_t i = 0; i < 8; i++) {
+        cell_voltages[i] = mapAnalog(cell_voltage_pins[i], ZERO_VOLTS, OTHER_VOLTS, ZERO_VOLTS_ANALOG, OTHER_VOLTS_ANALOG);
+    }
+    for (uint8_t i = 0; i < 8; i++) {
+        if (cell_voltages[i] <= CELL_CRITICAL_THRESHOLD) {
+            errorCellCritical();
+        }
+    }
+    for (uint8_t i = 0; i < 8; i++) {
+        if (cell_voltages[i] <= CELL_UNDERVOLT_THRESHOLD) {
+            errorCellUndervoltage();
+        }
+    }
+}
+
+//FUNCTIONS//
+
+float mapAnalog(uint8_t pin, float units1, float units2, uint16_t analog1, uint16_t analog2) {
+    float m = (units2 - units1) / (analog2 - analog1);
+    return ((analogRead(pin) - analog1) * m) + units1;
+}
+
+void eStop() {
+    digitalWrite(LOGIC_SWITCH_INPUT, HIGH);
+    //beep bc bms is on, but everything else off
+    while (true) {
+        // smoke detector beep pattern
+        beep on
+        delay(1000); //check with malikai
+        beep off
+        delay(30000);
+
+        // Check if cell goes critical?
+    }
+}
+
+void roverRestart() {
+    digitalWrite(LOGIC_SWITCH_INPUT, LOW);
+    beep // also check
+    delay(1000); //check with malikai
+    digitalWrite(LOGIC_SWITCH_INPUT, HIGH);
+    off beep // turn off buzzer
+}
+
+void roverSuicide() {
+    beep 
+    delay(200);
+    digitalWrite(GATE, HIGH);
+}
+
+void errorOvercurrent() {
+    RoveComm.write(RC_BMS_OVERCURRENT_DATA_ID, RC_BMS_OVERCURRENT_DATA_COUNT, /*overcurrent variable (temporary)*/);
+    uint32_t current_time = millis();
+    if ((current_time - lastOvercurrentErrorTimeStamp) >= TENTHOUSAND) {
+        roverRestart();
+        lastOvercurrentErrorTimeStamp = current_time;
+    } else {
+        roverSuicide(); //Call 988 :(
+    }
+}
+
+void errorCellUndervoltage() {
+    RoveComm.write(RC_BMS_CELLUNDERVOLT_DATA_ID, RC_BMS_CELLUNDERVOLT_DATA_COUNT, /*cellundervoltage variable (temporary)*/);
+    eStop();
+}
+
+void errorCellCritical() {
+    RoveComm.write(RC_BMS_CELLCRITICAL_DATA_ID, RC_BMS_CELLCRITICAL_DATA_COUNT);
+    roverSuicide(); //Call 988
+}
+
+void errorOverHeat() {
+    uint32_t current_time = millis();
+
+    if (current_time - lastOverheatWriteTimeStamp > TELEMETRY_PERIOD) {
+        RoveComm.write(RC_BMS_OVERHEAT_DATA_ID, RC_BMS_OVERHEAT_DATA_COUNT, temp);
+        lastOverheatWriteTimeStamp = current_time;
+    }
+
+    if (current_time - lastBuzzTimeStamp > notifyOverheat[notifyOverheatIndex]) {
+        digitalWrite(BUZZER, (notifyOverheatIndex%2));
+        lastBuzzTimeStamp = current_time;
+        notifyOverheatIndex++;
+        if (notifyOverheatIndex >= NOTIFYOVERHEAT_LENGTH) {
+            notifyOverheatIndex = 0;
+        }
+    }
 }
